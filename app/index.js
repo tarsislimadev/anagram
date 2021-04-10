1682a6cb992134dba10c3fc256e811cefb3651a1
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-10 00:29:04 -0300

    First release

---
+++ app/index.js
@@ -0,0 +1,70 @@
+
+const elHistory = document.getElementById('history')
+const elLetters = document.getElementById('letters')
+
+const words = ['nana', 'nene', 'nini', 'nono', 'nunu']
+const current = []
+const past = []
+
+const saveHistory = () => past.push(current)
+
+const showHistory = () => {
+  elHistory.innerHTML = ''
+  past.forEach(letters => {
+    const tr = document.createElement('tr')
+
+    const td = document.createElement('td')
+    td.innerText = letters.join('')
+
+    tr.appendChild(td)
+    elHistory.appendChild(tr)
+  })
+}
+
+const getWord = () => words[Math.floor(Math.random() * words.length)]
+
+const clearCurrent = () => { while (current[0]) current.shift() }
+
+const setCurrent = (letters) => letters.forEach(letter => current.push(letter))
+
+const getLetters = () => getWord().split('')
+
+let mouseY = null
+
+const onTouch = ({ type, target, changedTouches: [ { pageY: y } ] }) => {
+  const { innerText } = target
+
+  if (type === 'touchstart') {
+    mouseY = y
+  } else {
+    const charCode = innerText.charCodeAt() + (mouseY - y > 0 ? -1 : 1)
+    if (charCode > 122 || charCode < 97) return
+    target.innerText = String.fromCharCode(charCode)
+  }
+}
+
+const showLetters = () => {
+  elLetters.innerHTML = ''
+
+  const tr = document.createElement('tr')
+
+  current.forEach(letter => {
+    const td = document.createElement('td')
+    td.innerText = letter
+    td.addEventListener("touchstart", onTouch, false);
+    td.addEventListener("touchend", onTouch, false);
+    tr.appendChild(td)
+  })
+
+  elLetters.appendChild(tr)
+}
+
+const start = () => {
+  saveHistory()
+  showHistory()
+  clearCurrent()
+  setCurrent(getLetters())
+  showLetters()
+}
+
+document.addEventListener('DOMContentLoaded', () => start())
