e7969fde6211aef7d29d1ae6de732a6efd9df638
Tarsis de Lima <br.tmvdl@gmail.com>
2021-04-26 21:57:33 -0300

    Fixing project

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cordova platform add android
-
-cordova build android --release --buildConfig
-
-mkdir -p /builds
-
-cp /app/platforms/android/app/build/outputs/bundle/release/app-release.aab /builds
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cordova platform add browser
-
-cordova run browser
